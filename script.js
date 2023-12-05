140e75f680480263c8a977e87d40263d271c4400
Tito Mia <titomia@Titos-iMac.local>
2023-12-06 04:29:35 +0600

    a

---
+++ script.js
@@ -0,0 +1,3 @@
+const loginBtn = document.querySelector('#login-btn').addEventListener('click', function(){
+  const loginArea = document.querySelector('#log-in-area')
+  loginArea.style.display = "none";})
